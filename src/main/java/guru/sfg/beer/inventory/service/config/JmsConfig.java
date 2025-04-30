becede84d63ad08a0e33be51ab64da2d60e18569
sandeepmirashi <sandeep.sm83@gmail.com>
2025-04-30 12:12:35 +0530

    Implement New Inventory Listener. closes #5

--- src/main/java/guru/sfg/beer/inventory/service/config/JmsConfig.java
+++ src/main/java/guru/sfg/beer/inventory/service/config/JmsConfig.java
@@ -1,20 +1,36 @@
 package guru.sfg.beer.inventory.service.config;
 
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 
+import javax.jms.ConnectionFactory;
+
+@Slf4j
 @Configuration
 public class JmsConfig {
+    public static final String NEW_INVENTORY_QUEUE = "new-inventory";
 
     @Bean
-    public MessageConverter jacksonJmsMessageConvertor(){
+    public MessageConverter jacksonJmsMessageConvertor(ObjectMapper objectMapper){
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
+        converter.setObjectMapper(objectMapper);
         return converter;
+    }
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory,  MessageConverter messageConverter) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(messageConverter);
+        factory.setErrorHandler(t -> log.error("JMS Listener error", t));
+        return factory;
     }
 }
---
+++ src/main/java/guru/sfg/beer/inventory/service/services/NewInventoryListener.java
@@ -0,0 +1,30 @@
+package guru.sfg.beer.inventory.service.services;
+
+import guru.sfg.beer.inventory.service.config.JmsConfig;
+import guru.sfg.beer.inventory.service.domain.BeerInventory;
+import guru.sfg.beer.inventory.service.repositories.BeerInventoryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+import sfg.common.events.NewInventoryEvent;
+
+import javax.jms.JMSException;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class NewInventoryListener {
+    private final BeerInventoryRepository beerInventoryRepository;
+
+    @JmsListener(destination = JmsConfig.NEW_INVENTORY_QUEUE)
+    public void Listen(NewInventoryEvent event) throws JMSException {
+        log.debug("Got Inventory :" + event.toString());
+
+        beerInventoryRepository.save(BeerInventory.builder()
+                        .beerId(event.getBeerDto().getId())
+                        .upc(event.getBeerDto().getUpc())
+                        .quantityOnHand(event.getBeerDto().getQuantityOnHand())
+                        .build());
+    }
+}
